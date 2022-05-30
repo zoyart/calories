ca2788fb567c4e536d9c94543e89e3c1292b9508
Artur Tsoi <xzoyart@gmail.com>
2022-05-30 19:12:27 +0500

    another one

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@ Route::view('/', 'index')->name('index');
 
 // Register
 Route::get('/register', [UserController::class, 'registerForm'])->name('register.form');
-
+Route::post('/register/store', [UserController::class, 'store'])->name('register.store');
 
 // Login
 Route::get('/login', [UserController::class, 'loginForm'])->name('login.form');
+Route::post('/auth', [UserController::class, 'auth'])->name('auth');
