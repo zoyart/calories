74ff4c6fec9abbd63e4a6def01deb37a306a6f09
fdfdfdfda <kadirovdenis123@gmail.com>
2022-05-30 21:47:57 +0300

    added logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,16 @@
 <!-- Тут header сделаешь -->
 <div class="header">
-    <div class="header__buttons_container">
-        <div class="login_button_wrapper">
-            <a href="#" class="big_text login_button">Sign in</a>
-        </div>
-        <div class="login_button_wrapper">
-            <a href="#" class="big_text login_button">Sign up</a>
+    <div class="logo_container">
+        <img src="{{ asset('\resources\img\logo.png') }}" alt="" class="logo">
+    </div>
+    <div class="part_3_container">
+        <div class="header__buttons_container">
+            <div class="login_button_wrapper">
+                <a href="#" class="big_text login_button">Sign in</a>
+            </div>
+            <div class="login_button_wrapper">
+                <a href="#" class="big_text login_button">Sign up</a>
+            </div>
         </div>
     </div>
 </div>
