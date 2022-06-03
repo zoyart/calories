ec322801041e4ab935e2d29bcdcf1508be04d7ec
Artur Tsoi <xzoyart@gmail.com>
2022-06-03 11:19:51 +0500

    Create target date in registration

--- app/Http/Controllers/API/RegistrationController.php
+++ app/Http/Controllers/API/RegistrationController.php
@@ -4,25 +4,45 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Types\Float_;
 
 class RegistrationController extends Controller
 {
-    /**
-     * @OA\Get(
-     *     path="/target-date",
-     *     summary="Getting the target date",
-     *     tags={"Registration"},
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful operation",
-     *     ),
-     *     @OA\Response(
-     *         response="401",
-     *         description="Unauthorized user",
-     *     ),
-     * )
-     */
-    public function targetDate() {
-        return "hello";
+    public function targetDate(Request $request) {
+        $request->validate([
+            'weight' => 'required',
+            'purpose_weight' => 'required',
+            'purpose' => 'required',
+        ]);
+
+        $weight = (float) $request->weight;
+        $purpose_weight = (float) $request->purpose_weight;
+
+        if ($request->purpose === 'down' and $weight > $purpose_weight) {
+            $difference = $weight - $purpose_weight;
+        } elseif ($request->purpose === 'up' and $weight < $purpose_weight) {
+            $difference = $purpose_weight - $weight;
+        } else {
+            return response()->json('Некорректные данные');
+        }
+
+        $weightPerWeek = [
+            '0.1' => "",
+            '0.2' => "",
+            '0.3' => "",
+            '0.4' => "",
+            '0.5' => "",
+            '0.6' => "",
+            '0.7' => "",
+        ];
+
+        foreach ($weightPerWeek as $key => $value) {
+            $count = (integer) ($difference / (float) $key);
+            $date = date_create(date('Y-m-d'));
+            $date = date_modify($date, "{$count} week");
+            $weightPerWeek[$key] = date_format($date, 'j \of F Y');
+        }
+
+        return response()->json($weightPerWeek);
     }
 }
