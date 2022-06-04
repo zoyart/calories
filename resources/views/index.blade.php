14fbb9b42d414e099abb13484d2e4cc534927643
Artur Tsoi <xzoyart@gmail.com>
2022-06-04 15:47:15 +0500

    Create fetch

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,3 +12,17 @@
     </div>
 
 @endsection
+
+<script>
+    let reqUrl = "http://calories/api/target-date?weight=80&purpose_weight=75&purpose=down";
+    
+    function sendRequest(method, url) {
+        return fetch(url).then(response => {
+            return response.text()
+        })
+    }
+
+    sendRequest('GET', reqUrl)
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+</script>
