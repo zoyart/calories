fbb62358729abc1026d99f03713238b0d1ae5647
fdfdfdfda <kadirovdenis123@gmail.com>
2022-06-12 23:22:46 +0300

    fixed register form and others...

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,12 +1,102 @@
 @extends('layouts.layout')
 
 @section('head-title')
-    Регистрация
+Регистрация
 @endsection
 
 
 @section('content')
 
-Это регистрация
+
+
+
+
+
+<div class="container active">
+    <h1 class="very_big_text calories">Calories</h1>
+    <div class="form_container">
+        <h2 class="big_text form__text">SIGN IN</h2>
+        <form action="#">
+            <div class="log_wrapper">
+                <input value="" type="email" name="login" class="bold_text login log__input" placeholder="Email">
+            </div>
+            <div class="log_wrapper">
+                <input value="" type="password" name="pass" class="bold_text log__input" placeholder="Password">
+            </div>
+            <button type="submit" class="log_wrapper submit_wrapper">
+                <p class="bold_text submit_text">LOGIN</p>
+            </button>
+        </form>
+        <p class="normal_text forgot_text">Forgot <a href="#" class="link_text">Username / Password?</a></p>
+        <p class="normal_text between_text">or</p>
+        <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
+    </div>
+</div>
+<div class="container hidden">
+    <h1 class="very_big_text calories">Calories</h1>
+    <div class="form_container">
+        <h2 class="big_text form__text">SIGN UP</h2>
+        <form action="#">
+            <div class="log_wrapper">
+                <input type="text" name="name" class="bold_text login log__input" placeholder="Name">
+            </div>
+            <div class="log_wrapper">
+                <input type="email" name="login" class="bold_text login log__input" placeholder="Email">
+            </div>
+            <div div class="log_wrapper">
+                <input type="password" name="pass" class="bold_text log__input" placeholder="Password">
+            </div>
+            <div class="log_wrapper submit_wrapper">
+                <input class="bold_text submit__text" type="submit" value="REGISTER"></p>
+            </div>
+        </form>
+        <p class="normal_text forgot_text"><a href="#" class="link_text">Or login to your account</a></p>
+    </div>
+</div>
+<div class="container hidden">
+    <h1 class="very_big_text calories">Calories</h1>
+    <div class="form_container">
+        <h2 class="big_text form__text"></h2>
+        <form action="#">
+            <div class="log_wrapper">
+                <input type="email" name="login" class="bold_text login log__input" placeholder="Email">
+            </div>
+            <div class="log_wrapper">
+                <input type="password" name="pass" class="bold_text log__input" placeholder="Password">
+            </div>
+            <div class="log_wrapper submit_wrapper">
+                <input class="bold_text submit__text" type="submit" value="LOGIN"></p>
+            </div>
+        </form>
+        <p class="normal_text forgot_text">Forgot <a href="#" class="link_text">Username / Password?</a></p>
+        <p class="normal_text between_text">or</p>
+        <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
+    </div>
+</div>
+<div class="container hidden">
+    <h1 class="very_big_text calories">Calories</h1>
+    <div class="form_container">
+        <h2 class="big_text form__text">Choose your gender:</h2>
+        <form name="gender">
+            <div class="genders">
+                <div class="man gender_wrapper">
+                    <button type="button" class="gender_button">
+                        <img src="{{ asset('\resources\img\bussiness-man.png') }}" class="gender_img" alt="Man">
+                    </button>
+                </div>
+                <div class="woman gender_wrapper">
+                    <button type="button" class="gender_button">
+                        <img src="{{ asset('\resources\img\businesswoman.png') }}" class="gender_img" alt="Woman">
+                    </button>
+                </div>
+            </div>
+            <input id="value_input" value="none" type="text" class="hidden">
+            <button type="submit" class="log_wrapper submit_wrapper">
+                <p class="bold_text submit_text">CONTINUE</p>
+            </button>
+        </form>
+    </div>
+</div>
+
 
 @endsection
