8d3e27b1a3b1038f1c2a34e3710113f783546234
fdfdfdfda <kadirovdenis123@gmail.com>
2022-06-23 14:56:52 +0300

    added transition script between forms

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,9 +7,9 @@ const genderBtns = document.querySelectorAll(".gender_button");
 const genderValue = document.getElementById("value_input");
 
 // Buttons function
-function buttons(buttons_list, active_class, passive_class) {
+function buttonsFun(buttons_list, active_class, passive_class) {
     for(let i=0;i<buttons_list.length;i++) {
-        buttons_list[i].addEventListener('click', (target) => {
+        buttons_list[i].addEventListener('click', () => {
             buttons_list[i].classList.toggle(active_class);
             for(let m=0;m<buttons_list.length;m++) {
                 if(buttons_list[m] != buttons_list[i]) {
@@ -22,5 +22,25 @@ function buttons(buttons_list, active_class, passive_class) {
     
 }
 
-buttons(activitiesBtns, "active_btn", "passive_btn");
-buttons(genderBtns, "active_btn", "passive_btn")
+buttonsFun(activitiesBtns, "active_btn", "passive_btn");
+buttonsFun(genderBtns, "active_btn", "passive_btn");
+
+// Transition of forms
+const submitBtns = document.querySelectorAll(".submit_wrapper");
+const pages = document.querySelectorAll(".container");
+
+function submitBtnsFun(sendButtons, pages) { 
+    for(let i=0;i<sendButtons.length;i++) {
+        sendButtons[i].addEventListener('click', () => { 
+            pages[i+1].classList.remove("hidden")
+            pages[i+1].classList.add("active")
+            for(let p=0; p<pages.length; p++) {
+                if(pages[p] != pages[i+1]) {
+                    pages[p].classList.add("hidden")
+                    pages[p].classList.remove("active")
+                }
+            }
+        })
+    }
+}
+submitBtnsFun(submitBtns, pages);
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -6,7 +6,7 @@
 
 
 @section('content')
-    <div class="container hidden">
+    <!-- <div class="container active">
         <h1 class="very_big_text calories">Calories</h1>
         <div class="form_container">
             <h2 class="big_text form__text">SIGN IN</h2>
@@ -25,29 +25,8 @@
             <p class="normal_text between_text">or</p>
             <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
         </div>
-    </div>
-    <div class="container hidden">
-        <h1 class="very_big_text calories">Calories</h1>
-        <div class="form_container">
-            <h2 class="big_text form__text">SIGN UP</h2>
-            <form action="#">
-                <div class="log_wrapper">
-                    <input type="text" name="name" class="bold_text login log__input" placeholder="Name">
-                </div>
-                <div class="log_wrapper">
-                    <input type="email" name="login" class="bold_text login log__input" placeholder="Email">
-                </div>
-                <div div class="log_wrapper">
-                    <input type="password" name="pass" class="bold_text log__input" placeholder="Password">
-                </div>
-                <div class="log_wrapper submit_wrapper">
-                    <input class="bold_text submit__text" type="submit" value="REGISTER"></p>
-                </div>
-            </form>
-            <p class="normal_text forgot_text"><a href="#" class="link_text">Or login to your account</a></p>
-        </div>
-    </div>
-    <div class="container hidden">
+    </div> -->
+    <!-- <div class="container hidden">
         <h1 class="very_big_text calories">Calories</h1>
         <div class="form_container">
             <h2 class="big_text form__text"></h2>
@@ -66,94 +45,116 @@
             <p class="normal_text between_text">or</p>
             <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
         </div>
-    </div>
-    <div class="container hidden">
-        <h1 class="very_big_text calories">Calories</h1>
-        <div class="form_container">
-            <h2 class="big_text form__text">Choose your gender:</h2>
-            <form name="gender">
-                <div class="genders">
-                    <div class="man gender_wrapper">
-                        <button type="button" class="gender_button">
-                            <img src="{{ asset('\resources\img\bussiness-man.png') }}" class="gender_img" alt="Man">
-                        </button>
+    </div> -->
+    <form id="main-form" action="">
+        <div class="container active">
+            <h1 class="very_big_text calories">Calories</h1>
+            <div class="form_container">
+                <h2 class="big_text form__text">Choose your gender:</h2>
+                <div class="form">
+                    <div class="genders">
+                        <div class="man gender_wrapper">
+                            <button type="button" class="gender_button">
+                                <img src="{{ asset('\resources\img\bussiness-man.png') }}" class="gender_img" alt="Man">
+                            </button>
+                        </div>
+                        <div class="woman gender_wrapper">
+                            <button type="button" class="gender_button">
+                                <img src="{{ asset('\resources\img\businesswoman.png') }}" class="gender_img" alt="Woman">
+                            </button>
+                        </div>
                     </div>
-                    <div class="woman gender_wrapper">
-                        <button type="button" class="gender_button">
-                            <img src="{{ asset('\resources\img\businesswoman.png') }}" class="gender_img" alt="Woman">
-                        </button>
-                    </div>
+                    <button class="log_wrapper submit_wrapper">
+                        <p class="bold_text submit_text">CONTINUE</p>
+                    </button>
                 </div>
-                <input id="value_input" value="none" type="text" class="hidden">
-                <button type="submit" class="log_wrapper submit_wrapper">
-                    <p class="bold_text submit_text">CONTINUE</p>
-                </button>
-            </form>
+            </div>
         </div>
-    </div>
-    <div id="activities" class="container active">
-        <h1 class="very_big_text calories">Calories</h1>
-        <div class="form_container">
-            <h2 class="big_text form__text">Select the level of daily activity</h2>
-            <form name="activity">
-                <div class="activities">
-                    <div id="first_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\sleep.png') }}" class="activities__img" alt="sleep">         
-                            <p class="normal_text activities__text">For sedentary people, there are few training or they are absent</p>                     
-                        </button>
+        <div class="container hidden">
+            <h1 class="very_big_text calories">Calories</h1>
+            <div class="form_container">
+                <h2 class="big_text form__text">Enter your data</h2>
+                <div class="form">
+                    <div class="log_wrapper">
+                        <input type="number" name="number" class="bold_text login log__input" placeholder="Your age">
                     </div>
-                    <div id="second_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\sports.png') }}" class="activities__img" alt="sleep">         
-                            <p class="normal_text activities__text">For people with low activity, easy training 1-3 times a week or in the form of a different activity equivalent</p>                     
-                        </button>
+                    <div class="log_wrapper">
+                        <input type="number" name="number" class="bold_text login log__input" placeholder="Your height">
                     </div>
-                    <div id="three_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\exercise.png') }}" class="activities__img" alt="sleep">         
-                            <p class="normal_text activities__text">For moderately active people: physical work of moderate gravity or regular training 3-5 days a week</p>                     
-                        </button>
+                    <div div class="log_wrapper">
+                        <input type="number" name="number" class="bold_text log__input" placeholder="Your weight">
                     </div>
-                    <div id="fourth_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\girya.png') }}" class="activities__img" alt="sleep">         
-                            <p class="normal_text activities__text">For very active people: physical work is full day or intensive training 6-7 times a week</p>                     
-                        </button>
-                    </div>
-                    <div id="fifth_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\fire.png') }}" class="activities__img" alt="sleep">         
-                            <p class="normal_text activities__text">For extremely active people: hard physical work and intensive training/playing sports</p>                     
-                        </button>
-                    </div>
+                    <button type="submit" class="log_wrapper submit_wrapper">
+                        <p class="bold_text submit_text">CONTINUE</p>
+                    </button>
                 </div>
-                <input id="activities-value_input" value="none" type="text" class="hidden">
-                <button type="submit" class="log_wrapper submit_wrapper">
-                    <p class="bold_text submit_text">CONTINUE</p>
-                </button>
-            </form>
+                <!-- <p class="normal_text forgot_text"><a href="#" class="link_text">Or login to your account</a></p> -->
+            </div>
         </div>
-    </div>
-    <div class="container hidden">
-        <h1 class="very_big_text calories">Calories</h1>
-        <div class="form_container">
-            <h2 class="big_text form__text">Enter your data</h2>
-            <form action="#">
-                <div class="log_wrapper">
-                    <input type="number" name="number" class="bold_text login log__input" placeholder="Your age">
+        <div id="activities" class="container hidden">
+            <h1 class="very_big_text calories">Calories</h1>
+            <div class="form_container">
+                <h2 class="big_text form__text">Select the level of daily activity</h2>
+                <div class="form" name="activity">
+                    <div class="activities">
+                        <div id="first_active-level" class="activities__div">
+                            <button type="button" class="activities__btn">
+                                <img src="{{ asset('\resources\img\sleep.png') }}" class="activities__img" alt="sleep">         
+                                <p class="normal_text activities__text">For sedentary people, there are few training or they are absent</p>                     
+                            </button>
+                        </div>
+                        <div id="second_active-level" class="activities__div">
+                            <button type="button" class="activities__btn">
+                                <img src="{{ asset('\resources\img\sports.png') }}" class="activities__img" alt="sleep">         
+                                <p class="normal_text activities__text">For people with low activity, easy training 1-3 times a week or in the form of a different activity equivalent</p>                     
+                            </button>
+                        </div>
+                        <div id="three_active-level" class="activities__div">
+                            <button type="button" class="activities__btn">
+                                <img src="{{ asset('\resources\img\exercise.png') }}" class="activities__img" alt="sleep">         
+                                <p class="normal_text activities__text">For moderately active people: physical work of moderate gravity or regular training 3-5 days a week</p>                     
+                            </button>
+                        </div>
+                        <div id="fourth_active-level" class="activities__div">
+                            <button type="button" class="activities__btn">
+                                <img src="{{ asset('\resources\img\girya.png') }}" class="activities__img" alt="sleep">         
+                                <p class="normal_text activities__text">For very active people: physical work is full day or intensive training 6-7 times a week</p>                     
+                            </button>
+                        </div>
+                        <div id="fifth_active-level" class="activities__div">
+                            <button type="button" class="activities__btn">
+                                <img src="{{ asset('\resources\img\fire.png') }}" class="activities__img" alt="sleep">         
+                                <p class="normal_text activities__text">For extremely active people: hard physical work and intensive training/playing sports</p>                     
+                            </button>
+                        </div>
+                    </div>
+                    <input id="activities-value_input" value="none" type="text" class="hidden">
+                    <button type="submit" class="log_wrapper submit_wrapper">
+                        <p class="bold_text submit_text">CONTINUE</p>
+                    </button>
                 </div>
-                <div class="log_wrapper">
-                    <input type="number" name="number" class="bold_text login log__input" placeholder="Your height">
-                </div>
-                <div div class="log_wrapper">
-                    <input type="number" name="number" class="bold_text log__input" placeholder="Your weight">
-                </div>
-                <button type="submit" class="log_wrapper submit_wrapper">
-                    <p class="bold_text submit_text">CONTINUE</p>
-                </button>
-            </form>
-            <!-- <p class="normal_text forgot_text"><a href="#" class="link_text">Or login to your account</a></p> -->
+            </div>
         </div>
-    </div>
+        <div class="container hidden">
+            <h1 class="very_big_text calories">Calories</h1>
+            <div class="form_container">
+                <h2 class="big_text form__text">SIGN UP</h2>
+                <div class="form" action="#">
+                    <div class="log_wrapper">
+                        <input type="text" name="name" class="bold_text login log__input" placeholder="Name">
+                    </div>
+                    <div class="log_wrapper">
+                        <input type="email" name="login" class="bold_text login log__input" placeholder="Email">
+                    </div>
+                    <div div class="log_wrapper">
+                        <input type="password" name="pass" class="bold_text log__input" placeholder="Password">
+                    </div>
+                    <div class="log_wrapper submit_wrapper">
+                        <input class="bold_text submit_text" type="submit" value="REGISTER"></p>
+                    </div>
+                </div>
+                <p class="normal_text forgot_text"><a href="#" class="link_text">Or login to your account</a></p>
+            </div>
+        </div>
+    </form>
 @endsection
