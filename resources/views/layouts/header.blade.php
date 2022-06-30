afa27a58ef787915d095027f59cfe5af10f58faf
fdfdfdfda <kadirovdenis123@gmail.com>
2022-06-30 10:16:13 +0300

    fixed problems with gamburger

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,13 +3,14 @@
         <img src="{{ asset('\resources\img\logo.png') }}" alt="" class="header__logo">
     </div>
     <nav class="header__nav_container">
-        <div class="header__text_wrapper">
-            <a href="#" class="normal_text header__text">Calories</a>
-            <a href="#" class="normal_text header__text">Recipes</a>
-            <a href="#" class="normal_text header__text">Progress</a>
-        </div>    
-    </nav>
-    <div class="hamburger-menu">
+        <div class="nav__wrapper">
+            <div class="header__text_wrapper">
+                <a href="#" class="normal_text header__text">Calories</a>
+                <a href="#" class="normal_text header__text">Recipes</a>
+                <a href="#" class="normal_text header__text">Progress</a>
+            </div> 
+        </div>   
+        <div class="hamburger-menu">
         <input id="menu__toggle" type="checkbox" />
         <label class="menu__btn" for="menu__toggle">
             <span></span>
@@ -20,6 +21,8 @@
             <a href="#" class="big_text header__text menu__item">Progress</a>
         </ul>
     </div>
+    </nav>
+    
     <div class="header__login_container">
         <a href="{{ route('login.form') }}" class="login_button_wrapper">
             <p class="bold_text login_button">Sign in</p>
