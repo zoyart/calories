ec48727408b437a96dac2edb343e6c64dd0c3da5
fdfdfdfda <kadirovdenis123@gmail.com>
2022-07-14 10:32:38 +0300

    made login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -6,7 +6,32 @@
 
 
 @section('content')
-    <div class="container">
+    <div class="container active">
+        <h1 class="very_big_text calories">Calories</h1>
+        <div class="form_container">
+            <h2 class="big_text form__text">SIGN IN</h2>
+            <form method="post" action="{{ route('auth') }}">
+                @csrf
+                <div class="log_wrapper">
+                    <input type="email" class="bold_text login log__input" id="email" aria-describedby="emailHelp" name="email" placeholder="Email">
+                </div>
+                <div class="log_wrapper">
+                    <input type="password" class="bold_text log__input" id="password" name="password" placeholder="Password">
+                </div>
+                <button type="submit" class="log_wrapper submit_wrapper">
+                    <p class="bold_text submit_text">LOGIN</p>
+                </button>
+            </form>
+            <p class="normal_text forgot_text">Forgot <a href="#" class="link_text">Username / Password?</a></p>
+            <p class="normal_text between_text">or</p>
+            <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
+        </div>
+    </div>
+    
+
+@endsection
+
+{{-- <div class="container">
         <form method="post" action="{{ route('auth') }}">
             @csrf
             <div class="mb-3">
@@ -17,12 +42,35 @@
                 <label for="password" class="form-label">Password</label>
                 <input type="password" class="form-control" id="password" name="password">
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="remember_me" name="remember_me">
-                <label class="form-check-label" for="remember_me">Check me out</label>
-            </div>
             <button type="submit" class="btn btn-outline-primary">Login</button>
         </form>
     </div>
+ --}}
 
-@endsection
+
+
+
+
+
+
+
+{{-- <div class="container hidden">
+        <h1 class="very_big_text calories">Calories</h1>
+        <div class="form_container">
+            <h2 class="big_text form__text"></h2>
+            <form action="#">
+                <div class="log_wrapper">
+                    <input type="email" name="login" class="bold_text login log__input" placeholder="Email">
+                </div>
+                <div class="log_wrapper">
+                    <input type="password" name="pass" class="bold_text log__input" placeholder="Password">
+                </div>
+                <div class="log_wrapper submit_wrapper">
+                    <input class="bold_text submit__text" type="submit" value="LOGIN"></p>
+                </div>
+            </form>
+            <p class="normal_text forgot_text">Forgot <a href="#" class="link_text">Username / Password?</a></p>
+            <p class="normal_text between_text">or</p>
+            <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
+        </div>
+    </div> --}}
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,51 +1,10 @@
 @extends('layouts.layout')
-
 @section('head-title')
     Регистрация
 @endsection
 
 
 @section('content')
-    <!-- <div class="container active">
-        <h1 class="very_big_text calories">Calories</h1>
-        <div class="form_container">
-            <h2 class="big_text form__text">SIGN IN</h2>
-            <form action="#">
-                <div class="log_wrapper">
-                    <input value="" type="email" name="login" class="bold_text login log__input" placeholder="Email">
-                </div>
-                <div class="log_wrapper">
-                    <input value="" type="password" name="pass" class="bold_text log__input" placeholder="Password">
-                </div>
-                <button type="submit" class="log_wrapper submit_wrapper">
-                    <p class="bold_text submit_text">LOGIN</p>
-                </button>
-            </form>
-            <p class="normal_text forgot_text">Forgot <a href="#" class="link_text">Username / Password?</a></p>
-            <p class="normal_text between_text">or</p>
-            <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
-        </div>
-    </div> -->
-    <!-- <div class="container hidden">
-        <h1 class="very_big_text calories">Calories</h1>
-        <div class="form_container">
-            <h2 class="big_text form__text"></h2>
-            <form action="#">
-                <div class="log_wrapper">
-                    <input type="email" name="login" class="bold_text login log__input" placeholder="Email">
-                </div>
-                <div class="log_wrapper">
-                    <input type="password" name="pass" class="bold_text log__input" placeholder="Password">
-                </div>
-                <div class="log_wrapper submit_wrapper">
-                    <input class="bold_text submit__text" type="submit" value="LOGIN"></p>
-                </div>
-            </form>
-            <p class="normal_text forgot_text">Forgot <a href="#" class="link_text">Username / Password?</a></p>
-            <p class="normal_text between_text">or</p>
-            <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
-        </div>
-    </div> -->
     @if($errors)
 
     @endif
