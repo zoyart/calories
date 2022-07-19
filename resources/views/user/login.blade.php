53a28ebc497343fcc17df1ad79a3db5267c83e2b
Artur Tsoy <xzoyart@gmail.com>
2022-07-19 14:22:31 +0500

    chart created

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -3,54 +3,55 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="_token" content="{{ csrf_token() }}">
     <title>
         @yield('head-title')
     </title>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('\resources\css\style.css') }}">
 </head>
 <body>
-    <div class="container active">
-        <h1 class="very_big_text calories">Calories</h1>
-        <div class="form_container">
-            <h2 class="big_text form__text">SIGN IN</h2>
-            <form method="post" action="{{ route('auth') }}" class="form">
-                @csrf
-                <div class="log_wrapper center_log">
-                    <input type="email" class="bold_text login log__input" id="email" aria-describedby="emailHelp" name="email" placeholder="Email">
-                </div>
-                <div class="log_wrapper center_log">
-                    <input type="password" class="bold_text log__input" id="password" name="password" placeholder="Password">
-                </div>
-                <button type="submit" class="log_wrapper submit_wrapper">
-                    <p class="bold_text submit_text">LOGIN</p>
-                </button>
-            </form>
-            <p class="normal_text forgot_text">Forgot <a href="#" class="link_text">Username / Password?</a></p>
-            <p class="normal_text between_text">or</p>
-            <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
-        </div>
+<div class="container active">
+    <h1 class="very_big_text calories">Calories</h1>
+    <div class="form_container">
+        <h2 class="big_text form__text">SIGN IN</h2>
+        <form method="post" action="{{ route('auth') }}" class="form">
+            @csrf
+            <div class="log_wrapper center_log">
+                <input type="email" class="bold_text login log__input" id="email" aria-describedby="emailHelp"
+                       name="email" placeholder="Email">
+            </div>
+            <div class="log_wrapper center_log">
+                <input type="password" class="bold_text log__input" id="password" name="password"
+                       placeholder="Password">
+            </div>
+            <button type="submit" class="log_wrapper submit_wrapper">
+                <p class="bold_text submit_text">LOGIN</p>
+            </button>
+        </form>
+        <p class="normal_text forgot_text">Forgot <a href="#" class="link_text">Username / Password?</a></p>
+        <p class="normal_text between_text">or</p>
+        <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
     </div>
-<<<<<<< HEAD
+</div>
 </form>
-    
-=======
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-            crossorigin="anonymous">
-    </script>
-    <script src="{{ asset('\resources\js\script.js') }}">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-            crossorigin="anonymous">
-    </script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous">
+</script>
+<script src="{{ asset('\resources\js\script.js') }}">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous">
+</script>
 </body>
->>>>>>> fbaa1c26aa0d216b5340a21c24b75d6595b0f41c
 
 
 {{-- <div class="container">
@@ -96,7 +97,8 @@
             <p class="normal_text"><a href="#" class="link_text">Create your Account</a></p>
         </div>
     </div> --}}
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
 </script>
 <script src="{{ asset('\resources\js\script.js') }}">
 </script>
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,13 +1,5 @@
-
-@section('head-title')
-    Регистрация
-@endsection
 <link rel="stylesheet" type="text/css" href="{{ asset('\resources\css\style.css') }}">
 
-
-@if($errors)
-
-@endif
 <form id="main-form" action="{{ route('register.store') }}" method="POST" class="container_form">
     @csrf
     <div class="container active">
@@ -16,16 +8,21 @@
             <h2 class="big_text form__text">Choose your gender:</h2>
             <div class="form">
                 <div class="genders">
-                    <div class="man gender_wrapper">
-                        <button type="button" class="gender_button">
-                            <img src="{{ asset('\resources\img\bussiness-man.png') }}" class="gender_img" alt="Man">
-                        </button>
-                    </div>
-                    <div class="woman gender_wrapper">
-                        <button type="button" class="gender_button">
-                            <img src="{{ asset('\resources\img\businesswoman.png') }}" class="gender_img" alt="Woman">
-                        </button>
-                    </div>
+
+                    {{--                    <div class="man gender_wrapper">--}}
+                    {{--                        <button type="button" class="gender_button">--}}
+                    {{--                            <img src="{{ asset('\resources\img\bussiness-man.png') }}" class="gender_img" alt="Man">--}}
+                    {{--                        </button>--}}
+                    {{--                    </div>--}}
+                    {{--                    <div class="woman gender_wrapper">--}}
+                    {{--                        <button type="button" class="gender_button">--}}
+                    {{--                            <img src="{{ asset('\resources\img\businesswoman.png') }}" class="gender_img" alt="Woman">--}}
+                    {{--                        </button>--}}
+                    {{--                    </div>--}}
+                    <select name="gender" id="gender" required>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
                 </div>
                 <button type="button" class="log_wrapper submit_wrapper">
                     <p class="bold_text submit_text">CONTINUE</p>
@@ -39,15 +36,20 @@
             <h2 class="big_text form__text">Enter your data</h2>
             <div class="form">
                 <div class="log_wrapper">
-                    <input type="number" name="age" class="bold_text login log__input" placeholder="Your age">
+                    <input type="number" name="age" class="bold_text login log__input" placeholder="Age" required>
                 </div>
                 <div class="log_wrapper">
-                    <input type="number" name="growth" class="bold_text login log__input" placeholder="Your height">
+                    <input type="number" name="growth" class="bold_text login log__input" placeholder="Growth" required>
                 </div>
                 <div div class="log_wrapper">
-                    <input type="number" name="weight" class="bold_text log__input" placeholder="Your weight">
+                    <input type="number" name="weight" id="weight" class="bold_text log__input" placeholder="Weight"
+                           required>
                 </div>
-                <button type="button" class="log_wrapper submit_wrapper">
+                <div div class="log_wrapper">
+                    <input type="number" name="purpose_weight" id="purpose_weight" class="bold_text log__input"
+                           placeholder="Purpose weight" required>
+                </div>
+                <button type="button" class="log_wrapper submit_wrapper" id="btn_section_two">
                     <p class="bold_text submit_text">CONTINUE</p>
                 </button>
             </div>
@@ -59,38 +61,45 @@
         <div class="form_container">
             <h2 class="big_text form__text">Select the level of daily activity</h2>
             <div class="form" name="activity">
-                <div class="activities">
-                    <div id="first_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\sleep.png') }}" class="activities__img" alt="sleep">
-                            <p class="normal_text activities__text">For sedentary people, there are few training or they are absent</p>
-                        </button>
-                    </div>
-                    <div id="second_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\sports.png') }}" class="activities__img" alt="sleep">
-                            <p class="normal_text activities__text">For people with low activity, easy training 1-3 times a week or in the form of a different activity equivalent</p>
-                        </button>
-                    </div>
-                    <div id="three_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\exercise.png') }}" class="activities__img" alt="sleep">
-                            <p class="normal_text activities__text">For moderately active people: physical work of moderate gravity or regular training 3-5 days a week</p>
-                        </button>
-                    </div>
-                    <div id="fourth_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\girya.png') }}" class="activities__img" alt="sleep">
-                            <p class="normal_text activities__text">For very active people: physical work is full day or intensive training 6-7 times a week</p>
-                        </button>
-                    </div>
-                    <div id="fifth_active-level" class="activities__div">
-                        <button type="button" class="activities__btn">
-                            <img src="{{ asset('\resources\img\fire.png') }}" class="activities__img" alt="sleep">
-                            <p class="normal_text activities__text">For extremely active people: hard physical work and intensive training/playing sports</p>
-                        </button>
-                    </div>
-                </div>
+                {{--                <div class="activities">--}}
+                {{--                    <div id="first_active-level" class="activities__div">--}}
+                {{--                        <button type="button" class="activities__btn">--}}
+                {{--                            <img src="{{ asset('\resources\img\sleep.png') }}" class="activities__img" alt="sleep">--}}
+                {{--                            <p class="normal_text activities__text">For sedentary people, there are few training or they are absent</p>--}}
+                {{--                        </button>--}}
+                {{--                    </div>--}}
+                {{--                    <div id="second_active-level" class="activities__div">--}}
+                {{--                        <button type="button" class="activities__btn">--}}
+                {{--                            <img src="{{ asset('\resources\img\sports.png') }}" class="activities__img" alt="sleep">--}}
+                {{--                            <p class="normal_text activities__text">For people with low activity, easy training 1-3 times a week or in the form of a different activity equivalent</p>--}}
+                {{--                        </button>--}}
+                {{--                    </div>--}}
+                {{--                    <div id="three_active-level" class="activities__div">--}}
+                {{--                        <button type="button" class="activities__btn">--}}
+                {{--                            <img src="{{ asset('\resources\img\exercise.png') }}" class="activities__img" alt="sleep">--}}
+                {{--                            <p class="normal_text activities__text">For moderately active people: physical work of moderate gravity or regular training 3-5 days a week</p>--}}
+                {{--                        </button>--}}
+                {{--                    </div>--}}
+                {{--                    <div id="fourth_active-level" class="activities__div">--}}
+                {{--                        <button type="button" class="activities__btn">--}}
+                {{--                            <img src="{{ asset('\resources\img\girya.png') }}" class="activities__img" alt="sleep">--}}
+                {{--                            <p class="normal_text activities__text">For very active people: physical work is full day or intensive training 6-7 times a week</p>--}}
+                {{--                        </button>--}}
+                {{--                    </div>--}}
+                {{--                    <div id="fifth_active-level" class="activities__div">--}}
+                {{--                        <button type="button" class="activities__btn">--}}
+                {{--                            <img src="{{ asset('\resources\img\fire.png') }}" class="activities__img" alt="sleep">--}}
+                {{--                            <p class="normal_text activities__text">For extremely active people: hard physical work and intensive training/playing sports</p>--}}
+                {{--                        </button>--}}
+                {{--                    </div>--}}
+                {{--                </div>--}}
+                <select name="workout" id="workout" required>
+                    <option value="no">No</option>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                    <option value="athlete">Athlete</option>
+                </select>
                 <input id="activities-value_input" value="none" type="text" class="hidden">
                 <button type="button" class="log_wrapper submit_wrapper">
                     <p class="bold_text submit_text">CONTINUE</p>
@@ -103,6 +112,17 @@
         <div class="form_container">
             <h2 class="big_text form__text">SIGN UP</h2>
             <div class="form" action="#">
+                <div>
+                    <label for="pace" class="form-label">Pace</label>
+                    <input type="range" class="form-range" min="0.1" max="0.7" step="0.1" id="pace" name="pace"
+                           oninput="fun1()">
+                    <input type="text" id="pace_range" value="0.4">
+                    <label for="pace" class="form-label">kg/week</label>
+
+                </div>
+                <div>
+                    <canvas id="target_date" height="300px" width="300px"></canvas>
+                </div>
                 <div class="log_wrapper">
                     <input type="text" name="name" class="bold_text login log__input" placeholder="Name">
                 </div>
@@ -116,16 +136,113 @@
                     <input class="bold_text submit_text" type="submit" value="REGISTER"></p>
                 </div>
             </div>
-            <p class="normal_text forgot_text">Or <a style="color:#62E48B;" href="#" class="link_text">login</a> to your account</p>
+            <p class="normal_text forgot_text">Or <a style="color:#62E48B;" href="#" class="link_text">login</a> to your
+                account</p>
         </div>
     </div>
 </form>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
 </script>
 <script src="{{ asset('\resources\js\script.js') }}">
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
         crossorigin="anonymous">
+</script>
+
+
+{{-- Подключение библиотеки для отображения графиков (Подключать перед использованием!) --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js">
+</script>
+<script>
+
+    // Запрос на сервер, после получения целевого и текущего веса
+    var button = document.querySelector('#btn_section_two');
+    button.addEventListener('click', function () {
+        const sendData = async (url) => {
+            // Обязательный токен для запроса
+            const token = $('meta[name=_token]').attr('content');
+            // Запрос
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "X-Requested-With": "XMLHttpRequest",
+                    "X-CSRF-Token": token,
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Ошибка запроса или сервера');
+            }
+
+            return await response.json();
+        }
+
+        sendData('http://calories/api/target-date?weight=80&purpose_weight=75').then((data) => {
+            window.targetDates = { dates: data };
+
+            // Переменные для графика
+            let weight = document.querySelector('#weight').value;
+            let purpose_weight = document.querySelector('#purpose_weight').value;
+            // Отображение графика при переходе
+            let canvas = document.querySelector('#target_date').getContext('2d');
+            let pace_value = pace.value;
+            let date = window.targetDates.dates;
+
+            window.canvasObj = new Chart(canvas, {
+                type: 'line',
+                data: {
+                    labels: ['Today', date[`${pace_value}`]],
+                    datasets: [{
+                        label: 'Tempo',
+                        data: [weight, purpose_weight],
+                        backgroundColor: [
+                            'white'
+                        ],
+                        borderColor: [
+                            '#41CD8C'
+                        ],
+                        borderWidth: 2
+                    }]
+                },
+                options: {},
+            })
+        });
+    });
+
+    // Работает при изменении range
+    function fun1() {
+        // Отображение текущего числа в range
+        var pace = document.getElementById('pace');
+        var pace_range = document.getElementById('pace_range');
+        pace_range.value = pace.value;
+
+
+        // Обновление графика
+        let weight = document.querySelector('#weight').value;
+        let purpose_weight = document.querySelector('#purpose_weight').value;
+        let pace_value = pace.value;
+        let date = window.targetDates.dates;
+
+        window.canvasObj.data = {
+            labels: ['Today', date[`${pace_value}`]],
+            datasets: [{
+                label: 'Tempo',
+                data: [weight, purpose_weight],
+                backgroundColor: [
+                    'white'
+                ],
+                borderColor: [
+                    '#41CD8C'
+                ],
+                borderWidth: 2
+            }]
+        }
+
+        canvasObj.update();
+    }
 </script>
